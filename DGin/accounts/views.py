bb6758659abfaf895afa5bcf2fff953581cf78c8
Youjinxx <flourishxx7@gmail.com>
2022-06-23 16:42:54 +0900

    로그인,로그아웃,회원가입

--- DGin/accounts/views.py
+++ DGin/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-
 from django.contrib import auth
+from django.contrib.auth.models import User
 
 def login(request):
     if request.method == 'POST':
@@ -22,4 +22,17 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect('main:landing')
+
+def signup(request):
+    # signup 으로 POST 요청이 왔을 때, 새로운 유저를 만드는 절차를 밟는다.
+    if request.method == 'POST':
+        # password와 confirm에 입력된 값이 같다면
+        if request.POST['password'] == request.POST['confirm']:
+            # user 객체를 새로 생성
+            user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+            # 로그인 한다
+            auth.login(request, user)
+            return redirect('/')
+    # signup으로 GET 요청이 왔을 때, 회원가입 화면을 띄워준다.
+    return render(request, 'signup.html')
 # Create your views here.
