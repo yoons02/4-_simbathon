fd685aadc52fbb698c14fb185c7bc18be7a041ea
Youjinxx <flourishxx7@gmail.com>
2022-06-23 16:27:57 +0900

    auth 회원가입 구현 전

--- DGin/DGin/settings.py
+++ DGin/DGin/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'main',
     'qna',
+    'accounts',
 ]
 
 MIDDLEWARE = [
---
+++ DGin/accounts/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+
+from django.contrib import auth
+
+def login(request):
+    if request.method == 'POST':
+        usr = request.POST['username']
+        pwd = request.POST['password']
+
+        user = auth.authenticate(request,username=usr,password=pwd)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('main:landing')
+
+        else:
+            return render(request,'login.html')
+
+    elif request.method == 'GET':
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('main:landing')
+# Create your views here.
