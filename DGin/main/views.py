521adcf15ca3f70fb7a66de89eeebbdba91846f9
ustar1210 <ays991210@naver.com>
2022-06-25 11:43:34 +0900

    [안유성]메인(랜딩)페이지 제작, 질문 리스트/디테일 페이지에 추천 수 나오게 수정, static 폴더 경로 에러 수정

--- DGin/main/views.py
+++ DGin/main/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
-
+from asyncio.windows_events import NULL
+from django.shortcuts import render, redirect, get_object_or_404
+from qna.models import Professor, Department, Major, Question, Answer, Profile
+from django.utils import timezone
+from django.core.paginator import Paginator
 # Create your views here.
+
 def landing(request):
-    return render(request, 'landing.html')
+    recommended_questions = Question.objects.all().order_by('-like')[:4]
+    latest_questions = Question.objects.all().order_by('-pub_date')[:4]
+    unsolved_questions = []
+    solved_questions = []
+    for q in latest_questions:
+        unsolved_questions.append(q)
+    for q in latest_questions:
+        answers = q.answers.all()
+        for a in answers:
+            if a.selection == True:
+                solved_questions.append(q)
+                unsolved_questions.remove(q)
+    return render(request, 'landing.html', {
+        'recommended_questions':recommended_questions,
+        'latest_questions':latest_questions,
+        'unsolved_questions':unsolved_questions,
+        'solved_questions':solved_questions,
+    })
